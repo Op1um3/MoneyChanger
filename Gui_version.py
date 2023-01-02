30cc6e8930b5c470a8173276b85c59b314cec8e5
Op1um3 <116778540+Op1um3@users.noreply.github.com>
2023-01-02 18:15:09 -0500

    I didn't finished to do a else for convert
    Have to finish the else if the convertion isn't workink
    Also finish the backround color

--- Gui_version.py
+++ Gui_version.py
@@ -1,4 +1,5 @@
-#TODO : Do better comments for the combobox section, put everything in black and add a good interface and round widget. Being able to import other curencies and cryptocurencies to exchange.
+"""TODO : Do better comments for the combobox section, put everything in black and add a good interface and round widget. Being able to import other curencies and cryptocurencies to exchange.
+use fixer for api and not exchangerate."""
 import tkinter as tk
 import tkinter.ttk as ttk
 import requests
@@ -17,14 +18,20 @@ def convert():
   rate = data["rates"][to_currency]
 
   # convert the amount and display the result
-  result = float(amount) * rate
+  if result = True:
+  result = round(float(amount) * rate)
   result_label.config(text=f"{amount} {from_currency} = {result} {to_currency}")
-
+  print ("Your amount is"+ "result")
+  else :
+  print ("only number accepted")
 # create the main window
 root = tk.Tk()
 root.title("Currency Converter")
-
-# create the widgets
+# main window size
+root.geometry("250x250")
+# main window backroud
+root.configure(bg="#2A2728")
+# create the widgets 
 from_combo = tk.ttk.Combobox(root)
 from_combo["values"] = ("USD", "EUR", "CAD", "GBP")
 from_combo.current(0)

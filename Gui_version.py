fd42502197ecdb7445d1e73849601367ad004bbd
Op1um3 <116778540+Op1um3@users.noreply.github.com>
2023-01-21 15:33:58 -0500

    fixed a bunch

--- Gui_version.py
+++ Gui_version.py
@@ -10,7 +10,7 @@ wrong_amount_displayed = False
 wrong_currency_displayed = False
 
 # available currencies list:
-currencies = ["USD", "EUR", "CAD", "GBP","JPY",""]
+currencies = ["USD", "EUR", "CAD", "GBP","JPY"]
 
 wrong_amount_displayed = False
 wrong_currency_displayed = False
@@ -42,13 +42,13 @@ def convert():
       # convert the amount and display the result
       result = round(float(amount) * rate, 2)
       result_label = customtkinter.CTkLabel(master=root,fg_color="#191919", width=140, height=30, corner_radius=7)
-      result_label.place(relx=0.03, rely=0.90, anchor='w')
+      result_label.place(relx=0.5, rely=0.90, anchor='s')
       result_label.configure(text=f"{amount} {from_currency} = {result} {to_currency}")
     else:
       if not wrong_currency_displayed:
         wrong_currency = customtkinter.CTkLabel(master=root, text="Enter a valid currency",
                                                 bg_color="#2A2728",fg_color="#a30016", width=120, height=30 ,corner_radius=8)
-        wrong_currency.place(relx=0.03, rely=0.90, anchor='w')
+        wrong_currency.place(relx=0.15, rely=0.90, anchor='w')
         wrong_currency_displayed = True
         try:
           root.after_cancel(wrong_currency_after_id)
@@ -59,7 +59,7 @@ def convert():
     if not wrong_amount_displayed:
       wrong_amount = customtkinter.CTkLabel(master=root, text='Enter a valid amount',
                                             bg_color="#2A2728",fg_color="#a30016", width=140, height=30, corner_radius=8)
-      wrong_amount.place(relx=0.03, rely=0.90, anchor='w')
+      wrong_amount.place(relx=0.15, rely=0.90, anchor='w')
       wrong_amount_displayed = True
       try:
         root.after_cancel(wrong_amount_after_id)
@@ -69,7 +69,7 @@ def convert():
 
 # create the main window
 root = customtkinter.CTk()
-root.title("Currency Converter")
+root.title("")
 
 # The root theme
 customtkinter.set_appearance_mode("dark")
@@ -77,8 +77,8 @@ customtkinter.set_default_color_theme("dark-blue")
 
 # main window size
 root.geometry("600x300")
-root.minsize(600,300)
-root.maxsize(600,300)
+root.minsize(200,300)
+root.maxsize(200,300)
 
 # create the widgets 
 from_combo = customtkinter.CTkComboBox(master=root, values = currencies)
@@ -95,7 +95,7 @@ convert_button = customtkinter.CTkButton(master=root, text="Convert", command=co
                                          width=140, height=30, corner_radius=8)
 
 # add the widgets to the window
-rel_x = 0.03
+rel_x = 0.15
 from_combo.place(relx = rel_x, rely=0.10, anchor='w')
 to_combo.place(relx = rel_x, rely=0.30, anchor='w')
 entry.place(relx = rel_x, rely=0.50, anchor='w') 
